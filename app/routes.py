cede7ff504eb9367b2747b4f519d786cba0d0e9f
Malo <meunier.malo03@gmail.com>
2025-02-05 15:25:17 +0100

    structure et page login

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,10 @@ def init_routes(app):
     @app.route('/')
     def index():
         return render_template('index.html')
+    
+    @app.route('/login')
+    def login():
+        return render_template('login.html')
 
     @app.route('/alerts')
     def alerts():
